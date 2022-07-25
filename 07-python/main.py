3fb26b89e5e6b30b05b5b66a22378f8057ffa148
Alex Overstreet <alex@overstreet.me>
2022-07-24 19:03:16 -0500

    Update Day Seven to only read the file once

--- 07-python/main.py
+++ 07-python/main.py
@@ -1,17 +1,21 @@
 FILE = "./input.txt"
 
-# Part One
-with open(FILE, "r") as f:
-    xs = [int(x) for x in f.read().split(",")]
+
+def part_one(xs):
     sums = []
     for i in range(max(xs)):
         sums.append(sum([abs(x - i) for x in xs]))
     print("Part One:", min(sums))
 
-# Part Two
-with open(FILE, "r") as f:
-    xs = [int(x) for x in f.read().split(",")]
+
+def part_two(xs):
     sums = []
     for i in range(max(xs)):
         sums.append(sum([sum(range(abs(x - i)+1)) for x in xs]))
     print("Part Two:", min(sums))
+
+
+with open(FILE, "r") as f:
+    xs = [int(x) for x in f.read().split(",")]
+    part_one(xs)
+    part_two(xs)
